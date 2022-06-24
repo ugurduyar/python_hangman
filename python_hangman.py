e0b7c7b71d42b7fb89796f64316375aeae0f3ede
ugurduyar <ismailugurduyar@gmail.com>
2022-06-24 16:27:28 +0300

    hangman style

--- python_hangman.py
+++ python_hangman.py
@@ -6,3 +6,70 @@ response = urlopen(url)
 data_json = json.loads(response.read())
 words = (data_json["data"])
 random_word = (random.choice(words))
+i = 0
+guess_count = 0
+hangman_stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+print(random_word)
+while i < len(random_word):
+    print("_ " * len(random_word))
+    guess = input("Make a guess! ")
+    print(hangman_stages[guess_count])
+    if guess_count == 6:
+        print("You lost")
+        break
+    print(guess_count)
+    if guess in random_word:
+        print("found!")
+    else:
+        print("Not found")
+        guess_count += 1
+

d32932ab7306ed99f7610f14cef9c42ca6beb210
ugurduyar <ismailugurduyar@gmail.com>
2022-06-24 14:29:47 +0300

    extract list from url

--- python_hangman.py
+++ python_hangman.py
@@ -1,1 +1,7 @@
-test = 123
+import random
+import json
+from urllib.request import urlopen
+url = "https://www.randomlists.com/data/words.json"
+response = urlopen(url)
+data_json = json.loads(response.read())
+print(data_json)

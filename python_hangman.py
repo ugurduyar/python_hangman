6c76bd1cfc9a83a61f1635025d4805a838ae74a0
ugurduyar <ismailugurduyar@gmail.com>
2022-06-24 19:07:55 +0300

    done

--- python_hangman.py
+++ python_hangman.py
@@ -1,46 +1,3 @@
-import random
-import json
-from urllib.request import urlopen
-url = "https://www.randomlists.com/data/words.json"
-response = urlopen(url)
-data_json = json.loads(response.read())
-words = (data_json["data"])
-random_word = random.choice(words)
-print(random_word)
-right_guesses = []
-wrong_guesses = []
-
-def right_wrong():
-    if guess in random_word:
-        right_guesses.append(guess)
-        print("Thats correct!")
-    else:
-        wrong_guesses.append(guess)
-        print("That was wrong!")
-
-
-while True:
-    if len(wrong_guesses) == 6:
-        break
-    guess = input("\nMake a guess! \n")
-    if guess in right_guesses or guess in wrong_guesses:
-        print("You've guessed it before!")
-    else:
-        right_wrong()
-
-    for guess in random_word:
-        if guess in right_guesses:
-            print(guess,end="")
-        else:
-            print(" _ ",end="")
-
-
-
-
-
-
-
-
 hangman_stages = ['''
   +---+
   |   |
@@ -91,3 +48,49 @@ hangman_stages = ['''
  / \  |
       |
 =========''']
+
+
+
+import random
+import json
+from time import sleep
+from urllib.request import urlopen
+url = "https://www.randomlists.com/data/words.json"
+response = urlopen(url)
+data_json = json.loads(response.read())
+words = (data_json["data"])
+random_word = random.choice(words)
+norepeat = list(dict.fromkeys(random_word))
+print(norepeat)
+print(random_word)
+right_guesses = []
+wrong_guesses = []
+
+def right_wrong():
+    if guess in random_word:
+        right_guesses.append(guess)
+        print("Thats correct!")
+    else:
+        wrong_guesses.append(guess)
+        print("That was wrong!")
+
+
+while True:
+    print(hangman_stages[len(wrong_guesses)])
+    if len(right_guesses) == len(norepeat):
+        print("Congratulations!!!")
+        break
+    if len(wrong_guesses) == 6:
+        print("You've killed him!")
+        break
+    guess = input("\nMake a guess! \n")
+    if guess in right_guesses or guess in wrong_guesses:
+        print("You've guessed it before!")
+    else:
+        right_wrong()
+
+    for guess in random_word:
+        if guess in right_guesses:
+            print(guess + " ",end="")
+        else:
+            print(" _ ",end="")
